da39205ea883d45e772f7ee328787ad8d6963dd4
Maria Fernanda <nanamyrita@gmail.com>
2025-08-22 02:16:27 -0300

    feat: alteração main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,12 @@ export default async function Home() {
     <>
     <Header />
     
-    <main className="p-6">
-      <h1 className="text-3x1 font-bold"> Filmes do Studio Ghibli </h1>
+    <main className="p-6  m-10">
+      <div className="itens-left w-[50%]">
+      <p> Main page | Fest 2025 </p>
+      <h1 className="text-5xl font-bold"> Filmes do Studio Ghibli </h1>
+      <p> Studios Ghibli ipsum dolor sit, amet consectetur adipisicing elit. Veniam adipisci doloribus, numquam eveniet assumenda laborum est iste dolores aliquid cupiditate non beatae error nisi at accusamus repellat! Totam, reiciendis vitae.</p>
+      </div>
     </main>
     </>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 export default function Header() {
   return (
-    <header className="bg-white text-white p-4 flex justify-between items-center">
-        <img className="w-30" src="/logo.png" alt="" />
+    <header className="bg-gray-500 text-white p-4 flex justify-between items-center">
+        <img className="w-30" src="/logo.svg" alt="logo" />
       
       
 
